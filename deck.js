30dc733951d4add49cbf813731d7abb183091ff7
michodgs25 <michodgs@gmail.com>
2021-03-04 16:48:11 +0000

    Add comments to deck.js

--- deck.js
+++ deck.js
@@ -1,3 +1,4 @@
+// Define card suits& value, global as used throughout programme
 const SUITS = ["♠", "♣", "♥", "♦"]
 const VALUES = [
     "A",
@@ -14,24 +15,24 @@ const VALUES = [
     "Q",
     "K"
 ]
-
+// create deck class& export to script.js file
 export default class Deck {
     constructor(cards = freshDeck()) {
         this.cards = cards
     }
-
+    // returns number of cards in current deck
     get numberOfCards() {
         return this.cards.length
     }
-
+    // removes card from display
     pop() {
         return this.cards.shift()
     }
-
+    // adds card to the deck
     push(card) {
         this.cards.push(card)
     }
-
+    // after each game, the deck is shuffled
     shuffle() {
         for (let i = this.numberOfCards - 1; i > 0; i--) {
             const newIndex = Math.floor(Math.random() * (i + 1))
@@ -42,16 +43,19 @@ export default class Deck {
     }
 }
 
+// create card class& attach suits& values via a constructor
 class Card {
     constructor(suit, value) {
         this.suit = suit
         this.value = value
     }
 
+    // call color function, use ternary operator to apply color black or red 
     get color() {
         return this.suit === "♣" || this.suit === "♠" ? "black" : "red"
     }
 
+    // call index.html document, target card div elements, attach colors
     getHTML() {
         const cardDiv = document.createElement("div")
         cardDiv.innerText = this.suit
@@ -61,6 +65,7 @@ class Card {
     }
 }
 
+// create fresh deck function, after each game, fresh deck is called
 function freshDeck() {
     return SUITS.flatMap(suit => {
         return VALUES.map(value => {
